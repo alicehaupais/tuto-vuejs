696e33edb021266ff4b0ee3edaa8f74d30e61e20
Alice Haupais <alice.haupais@accenture.com>
2019-12-05 16:30:48 +0100

    computed

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -2,14 +2,28 @@
     <div>
         <h1> Title 1 </h1>
         <h2> Title 2 </h2>
+        <span> Hello {{ message }} </span>
+        <p> Message : {{msg}} </p>
+        <p> Message computed : {{onlyFirstWord}} </p>
     </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+     data() {
+       return {
+          message: "World",
+          msg : "hello World"
+       }
+    },
+    computed : {
+        onlyFirstWord() {
+            return this.msg.split(' ')[0].toUpperCase();
+        }
+    }
+  }
 </script>
+
 
 <style>
 
