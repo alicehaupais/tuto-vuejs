b63ff74bfe48ea536dfb9d31eaa5ca0dce062187
Alice Haupais <alice.haupais@accenture.com>
2019-12-06 11:25:56 +0100

    Add Car component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,10 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <Hello firstName = 'John' lastName = 'Dupont' from = 'New York' />
-    <Hello firstName = 'John' lastName = 'Dupont' from = 'New York' />
-    <p>
+    <h1> Test </h1>
+    <Car> </Car>
+    <h1> Test </h1>
+    <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -28,12 +30,13 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
 import Hello from '@/components/Hello.vue'
+import Car from '@/components/Car.vue'
 
 export default {
   name: 'HelloWorld',
@@ -41,7 +44,8 @@ export default {
     msg: String
   },
   components : {
-    Hello
+    Hello,
+    Car
   }
 }
 </script>
