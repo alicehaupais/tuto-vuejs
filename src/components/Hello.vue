e059b87234be82f16ab104fb9cc9b1ca2eb2ccc1
Alice Haupais <alice.haupais@accenture.com>
2019-12-06 10:16:22 +0100

    Modify variable in input text

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-        <h1> Hello {{firstName}} {{lastName}} de {{from}} </h1>
+        <h1> {{message}} </h1>
+        <h2> Hello {{person.firstName}} {{person.lastName}}</h2>
+        <label> Firstname : <input text = "text" v-model = "person.firstName"/> </label>
+        <label> Lastname : <input text = "text" v-model = "person.lastName"/> </label>
+        <label> Message : <input text = "text" v-model = "message"/> </label>
     </div>
 </template>
 
 <script>
     export default {
         data () {
-
+            return {
+                person : {
+                    firstName : 'John',
+                    lastName : 'Doe'
+                },
+                message : 'Welcome'
+            }
+            
         },
         props : ['firstName','lastName','from'],
     }
